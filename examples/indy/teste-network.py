777271887f0112d7f3c266c732e22898605bd21e
matheus nascimento <matheusnascimentoti99@gmail.com>
2023-01-30 22:01:55 -0300

    feat: Image indy-node

--- examples/indy/teste-network.py
+++ examples/indy/teste-network.py
@@ -1,5 +1,4 @@
 from typing import List
-
 from fogbed import (
     FogbedExperiment, Container, Resources, Services,
     CloudResourceModel, EdgeResourceModel, FogResourceModel,
@@ -15,16 +14,21 @@ if(__name__=='__main__'):
     cloud   = exp.add_virtual_instance('cloud')
     fog   = exp.add_virtual_instance('fog',   FogResourceModel(max_cu=4, max_mu=512))
     d1 = Container('d1', ip='10.0.0.1', dimage='hyperledger/indy-core-baseci:0.0.4', resources=Resources.SMALL)
-    d2 = Container('d2', ip='10.0.0.2', dimage='ubuntu:trusty', resources=Resources.SMALL)
-
+    d2 = Container('d2', ip='10.0.0.2', dimage='hyperledger/indy-core-baseci:0.0.4', resources=Resources.SMALL)
+    d3 = Container('d3', ip='10.0.0.3', dimage='hyperledger/indy-core-baseci:0.0.4', resources=Resources.SMALL)
+    d4 = Container('d4', ip='10.0.0.4', dimage='hyperledger/indy-core-baseci:0.0.4', resources=Resources.SMALL)
     exp.add_docker(d1, cloud)
-    exp.add_docker(d2, fog)
+    exp.add_docker(d2, cloud)
+    exp.add_docker(d3, cloud)
+    exp.add_docker(d4, cloud)
 
-    exp.add_link(cloud, fog)
+
+
     try:
         exp.start() 
         print(d1.cmd('ifconfig'))
         print(d1.cmd(f'ping -c 4 {d2.ip}'))
+        exp.start_cli()
         input('Press any key...')
     except Exception as ex: 
         print(ex)
--- indy/images/baseimage/indy-core-baseci.ubuntu.dockerfile
+++ indy/images/baseimage/indy-core-baseci.ubuntu.dockerfile
@@ -11,3 +11,30 @@ COPY indy-core-repo.preferences /etc/apt/preferences.d/indy-core-repo
 COPY __VERSION_FILE__ /
 
 RUN indy_image_clean
+ARG uid=1000
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CE7709D068DB5E88 || \
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CE7709D068DB5E88
+ARG indy_stream=master
+RUN echo "deb https://repo.sovrin.org/deb xenial $indy_stream" >> /etc/apt/sources.list
+
+RUN useradd -ms /bin/bash -u $uid indy
+
+ARG indy_plenum_ver=1.12.1~dev989
+ARG indy_node_ver=1.12.1~dev1172
+ARG python3_indy_crypto_ver=0.4.5
+ARG indy_crypto_ver=0.4.5
+ARG python3_pyzmq_ver=18.1.0
+ARG python3_orderedset_ver=2.0
+ARG python3_psutil_ver=5.4.3
+ARG python3_pympler_ver=0.5
+
+RUN apt-get update -y && apt-get install -y \
+        python3-pyzmq=${python3_pyzmq_ver} \
+        indy-plenum=${indy_plenum_ver} \
+        indy-node=${indy_node_ver} \
+        python3-indy-crypto=${python3_indy_crypto_ver} \
+        libindy-crypto=${indy_crypto_ver} \
+        python3-orderedset=${python3_orderedset_ver} \
+        python3-psutil=${python3_psutil_ver} \
+        python3-pympler=${python3_pympler_ver} \
+        vim
