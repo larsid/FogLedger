a2a5105b2a7da5c1043e60520a04bbf25d272990
Matheus Nascimento <matheusnascimentoti99@gmail.com>
2023-02-23 23:31:51 -0300

    :construction: fead: Define did from indy-cli

--- dlts/teste-network.py
+++ dlts/teste-network.py
@@ -19,22 +19,22 @@ if(__name__=='__main__'):
     exp = FogbedExperiment()
 
      # Define Indy network in cloud
-    indyCloud = IndyBasic(exp=exp, number_nodes=8)
+    indyCloud = IndyBasic(exp=exp, number_nodes=3)
     cloud = exp.add_virtual_instance('cloud')
     ledgers, nodes = indyCloud.create_ledgers('cloud')
     create_links(cloud, ledgers)
-
+    exp.add_link(cloud, indyCloud.cli_instance)
     # Define Indy network in fog
-    indyFog = IndyBasic(exp=exp, number_nodes=4)
-    fog = exp.add_virtual_instance('fog')
-    ledgers, nodes = indyFog.create_ledgers('fog')
-    create_links(fog, ledgers)
+    # indyFog = IndyBasic(exp=exp, number_nodes=3)
+    # fog = exp.add_virtual_instance('fog')
+    # ledgers, nodes = indyFog.create_ledgers('fog')
+    # create_links(fog, ledgers)
 
     try:
         exp.start() 
 
         indyCloud.start_network()
-        indyFog.start_network()
+        # indyFog.start_network()
 
         exp.start_cli()
         input('Press any key...')
