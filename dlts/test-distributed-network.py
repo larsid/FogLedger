19c9199b3d84f0a3fd54f00292c72f23ddbf6f99
Matheus Nascimento <matheusnascimentoti99@gmail.com>
2023-02-27 23:21:29 -0300

    :construction: Function to return genesis_file

--- dlts/test-distributed-network.py
+++ dlts/test-distributed-network.py
@@ -4,7 +4,7 @@ from fogbed import (
     CloudResourceModel, EdgeResourceModel, FogResourceModel,VirtualInstance,
     setLogLevel, FogbedDistributedExperiment, Worker
 )
-
+import time
 from indy.indy import (IndyBasic)
 setLogLevel('info')
 
@@ -24,19 +24,28 @@ if(__name__=='__main__'):
     worker1 = exp.add_worker(ip='192.168.0.104')
     worker2 = exp.add_worker(ip='192.168.0.105')
     webserver = exp.add_virtual_instance('webserver')
-    
-    exp.add_docker(
-            container=Container(
+    webserverContainer = Container(
             name='webserver', 
             dimage='webserver',
-            ),
+            port_bindings={9000: 9000},
+            environment={
+                'MAX_FETCH':50000,
+                'RESYNC_TIME':120,
+                'REGISTER_NEW_DIDS':True,
+                'LEDGER_INSTANCE_NAME':"fogbed",
+                'INFO_SITE_TEXT':"Node Container @ GitHub",
+                'INFO_SITE_URL':"https://github.com/hyperledger/indy-node-container",
+                'LEDGER_SEED':"000000000000000000000000Steward1"
+                }
+            )
+    exp.add_docker(
+            container=webserverContainer,
             datacenter=webserver)
 
      # Define Indy network in cloud
     indyCloud = IndyBasic(exp=exp, number_nodes=8)
     ledgersCloud, _ = indyCloud.create_ledgers('cloud')
-    print(ledgersCloud[:len(ledgersCloud)//2])
-    print(ledgersCloud[len(ledgersCloud)//2:])
+
     add_datacenters_to_worker(worker1, [indyCloud.cli_instance])
     add_datacenters_to_worker(worker1, ledgersCloud[:len(ledgersCloud)//2])
     add_datacenters_to_worker(worker2, ledgersCloud[len(ledgersCloud)//2:])
@@ -45,7 +54,10 @@ if(__name__=='__main__'):
     try:
         exp.start() 
         indyCloud.start_network()
-        print(indyCloud.request_genesis_file())
+        webserverContainer.cmd(f'echo "{indyCloud.request_genesis_file()}" >> /var/lib/indy/fogbed/pool_transactions_genesis')
+        time.sleep(10)
+        print(webserverContainer.cmd('./scripts/start_webserver.sh'))
+        
         input('Press any key...')
     except Exception as ex: 
         print(ex)
