b7ba99900670599bb8bdbc4a62ae7c9e5cf6b1e6
Matheus Nascimento <matheusnascimentoti99@gmail.com>
2023-04-04 23:28:27 -0300

    :sparkles: example: Aries connection

--- dlts/test-distributed-aca-py-network.py
+++ dlts/test-distributed-aca-py-network.py
@@ -11,7 +11,6 @@ from indy.indy import (IndyBasic)
 setLogLevel('info')
 
 
-
 def add_datacenters_to_worker(worker: Worker, datacenters: List[VirtualInstance]):
     for device in datacenters:
         worker.add(device, reachable=True)
@@ -22,17 +21,17 @@ if (__name__ == '__main__'):
     exp = FogbedDistributedExperiment()
     worker1 = exp.add_worker(ip='192.168.0.102')
     worker2 = exp.add_worker(ip='192.168.0.105')
-    
+
     # Define Indy network in cloud
-    indyCloud = IndyBasic(exp=exp, trustees_path = 'indy/tmp/trustees.csv', prefix='cloud',  number_nodes=3)
+    indyCloud = IndyBasic(
+        exp=exp, trustees_path='indy/tmp/trustees.csv', prefix='cloud',  number_nodes=3)
 
     acaPy1 = Container(
         name='aca-py1',
         dimage='aca-py',
-        port_bindings={3002: 3002, 3001:3001},
+        port_bindings={3002: 3002, 3001: 3001},
         ports=[3002, 3001],
         environment={
-            'ACAPY_ENDPOINT': "http://aries-1:3002",
             'ACAPY_GENESIS_FILE': "/var/lib/indy/fogbed/pool_transactions_genesis",
             'ACAPY_LABEL': "Aries 1 Agent",
             'ACAPY_WALLET_KEY': "secret",
@@ -44,13 +43,12 @@ if (__name__ == '__main__'):
             f'/tmp/indy/cloud:/var/lib/indy/'
         ]
     )
-    acaPy2= Container(
+    acaPy2 = Container(
         name='aca-py2',
         dimage='aca-py',
-        port_bindings={3002: 3004, 3001:3003},
+        port_bindings={3002: 3004, 3001: 3003},
         ports=[3002, 3001],
         environment={
-            'ACAPY_ENDPOINT': "http://aries-1:3002",
             'ACAPY_GENESIS_FILE': "/var/lib/indy/fogbed/pool_transactions_genesis",
             'ACAPY_LABEL': "Aries 2 Agent",
             'ACAPY_WALLET_KEY': "secret",
@@ -67,17 +65,28 @@ if (__name__ == '__main__'):
     exp.add_docker(
         container=acaPy1,
         datacenter=edge1)
-    
+    exp.add_docker(
+        container=Container(
+            name='agent1',
+            dimage='app',
+            environment={
+                'AGENT_ADDR': acaPy1.ip,
+                'AGENT_PORT': 3001
+            }
+        ),
+        datacenter=edge1)
+
     exp.add_docker(
         container=acaPy2,
         datacenter=edge2)
-
     
+
     add_datacenters_to_worker(worker1, [indyCloud.cli_instance])
 
-
-    add_datacenters_to_worker(worker1, indyCloud.ledgers[:len(indyCloud.ledgers)//2])
-    add_datacenters_to_worker(worker2, indyCloud.ledgers[len(indyCloud.ledgers)//2:])
+    add_datacenters_to_worker(
+        worker1, indyCloud.ledgers[:len(indyCloud.ledgers)//2])
+    add_datacenters_to_worker(
+        worker2, indyCloud.ledgers[len(indyCloud.ledgers)//2:])
     add_datacenters_to_worker(worker1, [edge1])
     add_datacenters_to_worker(worker2, [edge2])
     exp.add_tunnel(worker1, worker2)
@@ -85,9 +94,9 @@ if (__name__ == '__main__'):
         exp.start()
         indyCloud.start_network()
         time.sleep(2)
-        acaPy1.cmd('aca-py start --admin 0.0.0.0 3001 --admin-insecure-mode --auto-accept-intro-invitation-requests --auto-accept-invites --auto-accept-requests --auto-ping-connection --auto-provision --debug-connections --inbound-transport http 0.0.0.0 3002 --log-level INFO --outbound-transport http --public-invites --wallet-name fogbed --wallet-type indy > output.log 2>&1 &')
-        acaPy2.cmd('aca-py start --admin 0.0.0.0 3001 --admin-insecure-mode --auto-accept-intro-invitation-requests --auto-accept-invites --auto-accept-requests --auto-ping-connection --auto-provision --debug-connections --inbound-transport http 0.0.0.0 3002 --log-level INFO --outbound-transport http --public-invites --wallet-name fogbed --wallet-type indy > output.log 2>&1 &')
-        
+        acaPy1.cmd(f'aca-py start --endpoint http://{acaPy1.ip}:3002 --admin 0.0.0.0 3001 --admin-insecure-mode --auto-accept-intro-invitation-requests --auto-accept-invites --auto-accept-requests --auto-ping-connection --auto-provision --debug-connections --inbound-transport http 0.0.0.0 3002 --log-level INFO --outbound-transport http --public-invites --wallet-name fogbed --wallet-type indy > output.log 2>&1 &')
+        acaPy2.cmd(f'aca-py start --endpoint http://{acaPy2.ip}:3002 --admin 0.0.0.0 3001 --admin-insecure-mode --auto-accept-intro-invitation-requests --auto-accept-invites --auto-accept-requests --auto-ping-connection --auto-provision --debug-connections --inbound-transport http 0.0.0.0 3002 --log-level INFO --outbound-transport http --public-invites --wallet-name fogbed --wallet-type indy > output.log 2>&1 &')
+
         input('Press any key...')
     except Exception as ex:
         print(ex)
--- dlts/test-local-aca-py-network.py
+++ dlts/test-local-aca-py-network.py
@@ -21,7 +21,8 @@ if (__name__ == '__main__'):
     exp = FogbedExperiment()
 
     # Define Indy network in cloud
-    indyCloud = IndyBasic(exp=exp, trustees_path = 'indy/tmp/trustees.csv', prefix='cloud',  number_nodes=3)
+    indyCloud = IndyBasic(
+        exp=exp, trustees_path='indy/tmp/trustees.csv', prefix='cloud',  number_nodes=3)
     edge1 = exp.add_virtual_instance('edge')
     edge2 = exp.add_virtual_instance('edge2')
 
@@ -29,11 +30,10 @@ if (__name__ == '__main__'):
     create_links(cloud, indyCloud.ledgers)
     exp.add_link(cloud, indyCloud.cli_instance)
 
-
     webserverContainer = Container(
         name='webserver',
         dimage='mnplima/fogbed-indy-webserver:latest',
-        port_bindings={8000: 9000, 6543:6543},
+        port_bindings={8000: 9000, 6543: 6543},
         ports=[8000, 6543],
         environment={
             'MAX_FETCH': 50000,
@@ -59,10 +59,9 @@ if (__name__ == '__main__'):
     acaPy1 = Container(
         name='aca-py1',
         dimage='aca-py',
-        port_bindings={3002: 3002, 3001:3001},
+        port_bindings={3002: 3002, 3001: 3001},
         ports=[3002, 3001],
         environment={
-            'ACAPY_ENDPOINT': "http://aries-1:3002",
             'ACAPY_GENESIS_URL': f"http://{webserverContainer.ip}:8000/genesis",
             'ACAPY_LABEL': "Aries 1 Agent",
             'ACAPY_WALLET_KEY': "secret",
@@ -71,13 +70,12 @@ if (__name__ == '__main__'):
             'AGENT_PORT': 3002
         }
     )
-    acaPy2= Container(
+    acaPy2 = Container(
         name='aca-py2',
         dimage='aca-py',
-        port_bindings={3002: 3004, 3001:3003},
+        port_bindings={3002: 3004, 3001: 3003},
         ports=[3002, 3001],
         environment={
-            'ACAPY_ENDPOINT': "http://aries-1:3002",
             'ACAPY_GENESIS_URL': f"http://{webserverContainer.ip}:8000/genesis",
             'ACAPY_LABEL': "Aries 2 Agent",
             'ACAPY_WALLET_KEY': "secret",
@@ -86,27 +84,57 @@ if (__name__ == '__main__'):
             'AGENT_PORT': 3002
         }
     )
+
+    agent1 = Container(
+        name='agent1',
+        dimage='app',
+        port_bindings={5001:5001},
+        ports=[5001],
+        environment={
+            'AGENT_ADDR': f"http://{acaPy1.ip}:3001",
+            'AGENT_KEY': "secret"
+        }
+    )
+
+    agent2 = Container(
+        name='agent2',
+        dimage='app',
+        port_bindings={5001:5002},
+        ports=[5001],
+        environment={
+            'AGENT_ADDR': f"http://{acaPy1.ip}:3001",
+            'AGENT_KEY': "secret"
+        }
+    )
+
     exp.add_docker(
         container=acaPy1,
         datacenter=edge1)
-    
+
+    exp.add_docker(
+        container=agent1,
+        datacenter=edge1
+    )
+
     exp.add_docker(
         container=acaPy2,
         datacenter=edge2)
-
+    exp.add_docker(
+        container=agent2,
+        datacenter=edge1
+    )
     try:
         exp.start()
 
         indyCloud.start_network()
-        print(webserverContainer.cmd('./scripts/start_webserver.sh > output.log 2>&1 &'))
+        print(webserverContainer.cmd(
+            './scripts/start_webserver.sh > output.log 2>&1 &'))
         time.sleep(2)
-        acaPy1.cmd('aca-py start --admin 0.0.0.0 3001 --admin-insecure-mode --auto-accept-intro-invitation-requests --auto-accept-invites --auto-accept-requests --auto-ping-connection --auto-provision --debug-connections --inbound-transport http 0.0.0.0 3002 --log-level INFO --outbound-transport http --public-invites --wallet-name fogbed --wallet-type indy > output.log 2>&1 &')
-        acaPy2.cmd('aca-py start --admin 0.0.0.0 3001 --admin-insecure-mode --auto-accept-intro-invitation-requests --auto-accept-invites --auto-accept-requests --auto-ping-connection --auto-provision --debug-connections --inbound-transport http 0.0.0.0 3002 --log-level INFO --outbound-transport http --public-invites --wallet-name fogbed --wallet-type indy > output.log 2>&1 &')
+        acaPy1.cmd(f'aca-py start --endpoint http://{acaPy1.ip}:3002 --admin 0.0.0.0 3001 --admin-insecure-mode --auto-accept-intro-invitation-requests --auto-accept-invites --auto-accept-requests --auto-ping-connection --auto-provision --debug-connections --inbound-transport http 0.0.0.0 3002 --log-level INFO --outbound-transport http --public-invites --wallet-name fogbed --wallet-type indy > output.log 2>&1 &')
+        acaPy2.cmd(f'aca-py start --endpoint http://{acaPy2.ip}:3002 --admin 0.0.0.0 3001 --admin-insecure-mode --auto-accept-intro-invitation-requests --auto-accept-invites --auto-accept-requests --auto-ping-connection --auto-provision --debug-connections --inbound-transport http 0.0.0.0 3002 --log-level INFO --outbound-transport http --public-invites --wallet-name fogbed --wallet-type indy > output.log 2>&1 &')
         exp.start_cli()
         input('Press any key...')
     except Exception as ex:
         print(ex)
     finally:
         exp.stop()
-
-
