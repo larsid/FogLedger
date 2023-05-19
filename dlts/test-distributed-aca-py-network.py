13c04003436eb65f65b5678084e77183bce5c766
matheusnascimentoti99 <matheusnascimentoti99@gmail.com>
2023-05-19 19:48:05 -0300

    :recycle: refactor: Add webserver to exp and change git action

--- dlts/test-distributed-aca-py-network.py
+++ dlts/test-distributed-aca-py-network.py
@@ -19,9 +19,34 @@ def add_datacenters_to_worker(worker: Worker, datacenters: List[VirtualInstance]
 if (__name__ == '__main__'):
 
     exp = FogbedDistributedExperiment()
-    worker1 = exp.add_worker('192.168.0.104')
-    worker2 = exp.add_worker('192.168.0.105')
+    worker1 = exp.add_worker('fog1')
+    worker2 = exp.add_worker('fog2')
+    cloud = exp.add_virtual_instance('cloud')
 
+    webserverContainer = Container(
+        name='webserver',
+        dimage='mnplima/fogbed-indy-webserver:latest',
+        port_bindings={8000: 80, 6543: 6543},
+        ports=[8000, 6543],
+        environment={
+            'MAX_FETCH': 50000,
+            'RESYNC_TIME': 120,
+            'WEB_ANALYTICS': True,
+            'REGISTER_NEW_DIDS': True,
+            'LEDGER_INSTANCE_NAME': "fogbed",
+            'INFO_SITE_TEXT': "Node Container @ GitHub",
+            'INFO_SITE_URL': "https://github.com/hyperledger/indy-node-container",
+            'LEDGER_SEED': "000000000000000000000000Trustee1",
+            'GENESIS_FILE': "/var/lib/indy/fogbed/pool_transactions_genesis"
+        },
+        volumes=[
+            f'/tmp/indy/cloud:/var/lib/indy/'
+        ]
+    )
+    instanceWebserver = exp.add_docker(
+        container=webserverContainer,
+        datacenter=cloud)
+    
     # Define Indy network in cloud
     indyCloud = IndyBasic(
         exp=exp, trustees_path='indy/tmp/trustees.csv', prefix='cloud',  number_nodes=4)
@@ -29,7 +54,7 @@ if (__name__ == '__main__'):
     acaPy1 = Container(
         name='aca-py1',
         dimage='aca-py',
-        port_bindings={3002: 3002, 3001: 3001},
+        port_bindings={3002: 3002, 3001: 8080},
         ports=[3002, 3001],
         environment={
             'ACAPY_GENESIS_FILE': "/pool_transactions_genesis",
@@ -43,7 +68,7 @@ if (__name__ == '__main__'):
     acaPy2 = Container(
         name='aca-py2',
         dimage='aca-py',
-        port_bindings={3002: 3004, 3001: 3003},
+        port_bindings={3002: 3004, 3001: 8080},
         ports=[3002, 3001],
         environment={
             'ACAPY_GENESIS_FILE': "/pool_transactions_genesis",
@@ -62,26 +87,27 @@ if (__name__ == '__main__'):
         container=acaPy2,
         datacenter=edge2)
 
-    agent1 = exp.add_virtual_instance('agent1')
-    agent2 = exp.add_virtual_instance('agent2')
     exp.add_docker(
         container=acaPy1,
         datacenter=edge1)
+    agent1 = exp.add_virtual_instance('agent1')
+    agent2 = exp.add_virtual_instance('agent2')
+    
     exp.add_docker(
-        container=Container(
-            name='agent1',
-            dimage='app',
-            environment={
-                'AGENT_ADDR': acaPy1.ip,
-                'AGENT_PORT': 3001
-            }
-        ),
+    container=Container(
+        name='agent1',
+        dimage='mnplima/fogbed-app-aca-py',
+        environment={
+            'AGENT_ADDR': acaPy1.ip,
+            'AGENT_PORT': 3001
+        }
+    ),
         datacenter=agent1)
     
     exp.add_docker(
         container=Container(
             name='agent2',
-            dimage='app',
+            dimage='mnplima/fogbed-app-aca-py',
             environment={
                 'AGENT_ADDR': acaPy2.ip,
                 'AGENT_PORT': 3001
@@ -89,18 +115,20 @@ if (__name__ == '__main__'):
         ),
         datacenter=agent2)
     
+    
     add_datacenters_to_worker(worker1, [indyCloud.cli_instance])
 
     add_datacenters_to_worker(
-        worker1, indyCloud.ledgers)
-    # add_datacenters_to_worker(
-    #     worker2, indyCloud.ledgers[len(indyCloud.ledgers)//2:])
-    add_datacenters_to_worker(worker1, [edge1, agent1])
+        worker1, indyCloud.ledgers[:len(indyCloud.ledgers)//2])
+    add_datacenters_to_worker(
+        worker2, indyCloud.ledgers[len(indyCloud.ledgers)//2:])
+    add_datacenters_to_worker(worker1, [edge1, cloud, agent1])
     add_datacenters_to_worker(worker2, [edge2, agent2])
     exp.add_tunnel(worker1, worker2)
     try:
         exp.start()
         indyCloud.start_network()
+        webserverContainer.cmd('./scripts/start_webserver.sh > output.log 2>&1 &')
         time.sleep(10)
         acaPy1.cmd(f"echo '{indyCloud.genesis_content}' > /pool_transactions_genesis")
         acaPy1.cmd(f'aca-py start \
--- dlts/test-distributed-network.py
+++ dlts/test-distributed-network.py
@@ -23,10 +23,34 @@ if (__name__ == '__main__'):
     worker1 = exp.add_worker('fog1')
     worker2 = exp.add_worker('fog2')
 
+    cloud = exp.add_virtual_instance('cloud')
+    webserverContainer = Container(
+        name='webserver',
+        dimage='mnplima/fogbed-indy-webserver:latest',
+        port_bindings={8000: 80, 6543: 6543},
+        ports=[8000, 6543],
+        environment={
+            'MAX_FETCH': 50000,
+            'RESYNC_TIME': 120,
+            'WEB_ANALYTICS': True,
+            'REGISTER_NEW_DIDS': True,
+            'LEDGER_INSTANCE_NAME': "fogbed",
+            'INFO_SITE_TEXT': "Node Container @ GitHub",
+            'INFO_SITE_URL': "https://github.com/hyperledger/indy-node-container",
+            'LEDGER_SEED': "000000000000000000000000Trustee1",
+            'GENESIS_FILE': "/var/lib/indy/fogbed/pool_transactions_genesis"
+        },
+        volumes=[
+            f'/tmp/indy/cloud:/var/lib/indy/'
+        ]
+    )
+    instanceWebserver = exp.add_docker(
+        container=webserverContainer,
+        datacenter=cloud)
     # Define Indy network in cloud
     indyCloud = IndyBasic(exp=exp, trustees_path = 'indy/tmp/trustees.csv', prefix='cloud',  number_nodes=3)
 
-    add_datacenters_to_worker(worker1, [indyCloud.cli_instance])
+    add_datacenters_to_worker(worker1, [indyCloud.cli_instance, cloud])
 
 
     add_datacenters_to_worker(worker1, indyCloud.ledgers[:len(indyCloud.ledgers)//2])
@@ -35,6 +59,7 @@ if (__name__ == '__main__'):
     try:
         exp.start()
         indyCloud.start_network()
+        print(webserverContainer.cmd('./scripts/start_webserver.sh > output.log 2>&1 &'))
         indyCloud.indy_cli.cmd(f"echo '{indyCloud.genesis_content}' > /pool_transactions_genesis")
         input('Press any key...')
     except Exception as ex:
