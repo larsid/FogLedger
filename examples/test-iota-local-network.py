5a276f37420f7e5b91f45fef255b53aae85ba03c
WesleiSantos <weslei200912@gmail.com>
2023-08-13 01:07:50 -0300

    :sparkles: finished installing iota explorer and local tests

--- examples/test-iota-local-network.py
+++ examples/test-iota-local-network.py
@@ -3,6 +3,7 @@ from fogledger.iota.NodeConfig import (NodeConfig)
 from fogledger.iota.CoordConfig import (CoordConfig)
 from fogledger.iota.SpammerConfig import (SpammerConfig)
 from fogledger.iota.ApiConfig import (ApiConfig)
+from fogledger.iota.WebAppConfig import (WebAppConfig)
 from typing import List
 from fogbed import (
     FogbedExperiment,
@@ -19,16 +20,16 @@ def create_links(cloud: VirtualInstance, devices: List[VirtualInstance]):
 if (__name__ == '__main__'):
     exp = FogbedExperiment()
  
-    node1 = NodeConfig(name='node1', port_bindings={'8081':'8081'})
+    node1 = NodeConfig(name='node1', port_bindings={'8081':'8081', '14265':'14265'})
     node2 = NodeConfig(name='node2', port_bindings={'8081':'8082'})
     node3 = NodeConfig(name='node3', port_bindings={'8081':'8083'})
     node4 = NodeConfig(name='node4', port_bindings={'8081':'8084'})
 
     cord = CoordConfig(name='cord', port_bindings={'8081':'8085'}, interval='60s')
     spammer = SpammerConfig(name='spammer', port_bindings={'8081':'8086'}, message ='one-click-tangle.')
-    api = ApiConfig(name='spammer', port_bindings={'8081':'8081'})
-
-    iota = IotaBasic(exp=exp, prefix='fog', conf_nodes=[node1, node2, node3, node4], conf_coord=cord, conf_spammer=spammer, conf_api=api)
+    api = ApiConfig(name='api', port_bindings={'4000':'4000'})
+    web_app = WebAppConfig(name='web_app', port_bindings={'80':'82'})
+    iota = IotaBasic(exp=exp, prefix='fog', conf_nodes=[node1, node2, node3, node4], conf_coord=cord, conf_spammer=spammer, conf_api=api, conf_web_app=web_app)
 
     fog = exp.add_virtual_instance('fog')
     create_links(fog, iota.ledgers)
